6dae20a0dd6ee47879ecf35239f20ec0070e3af4
Cool-Programmer-1 <sa721633@gmail.com>
2024-09-11 01:43:38 +0530

    code of webfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for navigation
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
